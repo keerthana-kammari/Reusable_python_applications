96247628bb77e6f147bf9512d115229645249cf0
keerthana-kammari <keerthanakammari@outlook.com>
2019-01-18 15:54:56 -0600

    close commands updated

--- regex_apps/find_email_phone.py
+++ regex_apps/find_email_phone.py
@@ -20,6 +20,7 @@ def check_text(input_file):
         print("List of mobile numbers from the uploaded file: ", match_mob)
     except FileNotFoundError:
         print("No file exists in the provided path!!")
+        data.close()
        
 #Extract function for .csv files     
 def check_csv(input_file):
@@ -35,6 +36,7 @@ def check_csv(input_file):
             print(match_mob)
     except FileNotFoundError:
         print("No file exists in the provided path!!")
+        csv_file.close()
        
 #Extract function for .docx files
 def check_doc(input_file):
@@ -53,6 +55,7 @@ def check_doc(input_file):
                 print(mob)
     except:
         print("No file exists in the provided path!!")
+        doc.close()
 
 if __name__ == "__main__":
     if bool(csv_pattern.match(sys.argv[1])) == True:
